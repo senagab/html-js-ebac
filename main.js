a584b60c1ea1ba8579f733e089735e825eb2bf80
gabriel sena <senagab@gmail.com>
2023-07-31 20:22:48 -0400

    feat: validação de form + documentação de funções

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 const form = document.getElementById('form-deposito');
 
 function validaNome(nomeCompleto){
+    // passa o nome inserido como parametro
+    
     const nomeComoArray = nomeCompleto.split(' ');
+    // recebe o nome inserido como array e splita
+
     return nomeComoArray.length >= 2;
+    // retorna (como true) o nome - como array - se forem pelo menos 2 nomes (sugerindo assim que o usuario inseriu pelo menos nome + sobrenome)
 }
 
 form.addEventListener('submit', function(evento){
+    let formValido = false;
+    // cria a variavel formValida com valor inicial setado em false
+    
     evento.preventDefault();
-
+    // método que quando chamado impede a execução da ação padrão associada ao evento
+    
     const nomeBeneficiario = document.getElementById('nome-beneficiario');
-    if (validaNome(nomeBeneficiario.value) === false) {
-        alert("o nome não está completo");
+    formValido = validaNome(nomeBeneficiario.value)
+    // o form sera valido apenas se o nome do beneficiario tiver nome e sobrenome
+    if (formValido) {
+        console.log("tudo certo");
     } else{
-        console.log("tudo certo")
+        alert("o nome não está completo");
     }
 })
 
