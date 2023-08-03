d15c607aa78b8e6378d8acd106399543556b2705
gabriel sena <senagab@gmail.com>
2023-08-02 21:21:47 -0400

    feat: mensagem de erro para nome incompleto, elevação da variavel 'nomeBeneficiario'

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const form = document.getElementById('form-deposito');
 // form é a varival referente ao elemento <form> de id 'form-deposito'
 
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+// nomeBeneficiario quando declarado antes do código o torna uma variavel global
+
 function validaNome(nomeCompleto){
     // passa o nome inserido como parametro
     
@@ -18,25 +21,28 @@ form.addEventListener('submit', function(evento){
     evento.preventDefault();
     // método que quando chamado impede a execução da ação padrão associada ao evento
     
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    // const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
 
-    const msgSucesso = `Montante de: ${valorDeposito.value} | Depositado para o cliente: ${nomeBeneficiario.value} | Conta: ${numeroContaBeneficiario.value}`; 
+    const msgSucesso = `Montante de: <b>${valorDeposito.value}</b> | Depositado para o cliente: <b>${nomeBeneficiario.value}</b> | Conta: <b>${numeroContaBeneficiario.value}</b>`; 
 
     formValido = validaNome(nomeBeneficiario.value)
     // o form sera valido apenas se o nome do beneficiario tiver nome e sobrenome
+    
     if (formValido) {
-        alert(msgSucesso);
+        // alert(msgSucesso);
+        const containerMsgSucesso = document.querySelector('.success-message');
+        containerMsgSucesso.innerHTML = msgSucesso;
+        containerMsgSucesso.style.display = 'block';
 
         nomeBeneficiario.value = '';
         numeroContaBeneficiario.value = '';
         valorDeposito.value = '';
 
     } else{
-        alert("o nome não está completo");
+        nomeBeneficiario.style.border = "1px solid red";
+        document.querySelector('.error-message').style.display = 'block';
     }
 })
-
-console.log(form)
 
