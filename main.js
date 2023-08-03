1cb0561d2f6ba11703eeb1e346f40d7cd43d6fa1
gabriel sena <senagab@gmail.com>
2023-08-02 21:00:24 -0400

    fix: alterações finais no formulário: submit sem reload, reset de campos, retorno com valores inseridos pelo usuário

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form-deposito');
+// form é a varival referente ao elemento <form> de id 'form-deposito'
 
 function validaNome(nomeCompleto){
     // passa o nome inserido como parametro
@@ -18,10 +19,20 @@ form.addEventListener('submit', function(evento){
     // método que quando chamado impede a execução da ação padrão associada ao evento
     
     const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+
+    const msgSucesso = `Montante de: ${valorDeposito.value} | Depositado para o cliente: ${nomeBeneficiario.value} | Conta: ${numeroContaBeneficiario.value}`; 
+
     formValido = validaNome(nomeBeneficiario.value)
     // o form sera valido apenas se o nome do beneficiario tiver nome e sobrenome
     if (formValido) {
-        console.log("tudo certo");
+        alert(msgSucesso);
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+
     } else{
         alert("o nome não está completo");
     }
